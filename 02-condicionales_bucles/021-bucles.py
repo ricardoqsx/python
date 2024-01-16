f77b2225d10b33bdf506138cff3d7d58a99e1e62
ricardoqsx <ricardoqs@me.com>
2024-01-16 12:17:48 -0500

    adding bucles

--- 02-condicionales_bucles/021-bucles.py
+++ 02-condicionales_bucles/021-bucles.py
@@ -1,4 +1,6 @@
- # Bucles
+import math
+
+# Bucles
 # existen 2 tipos
 
 # bucle determinado
@@ -11,7 +13,7 @@
 # while condicion:
 #     print("cuerpo del bucle")
 
-# c=0
+# c=0 
 
 """while c<10:
     print("Hola")
@@ -19,14 +21,34 @@
 print("afuera del bucle") """
 
 cc=1
-edad=(int(input("Escribe tu edad: ")))
+edad=(int(input("Escribe tu edad por favor: ")))
 
 while edad<0 or edad>100 :
-    print("Edad negativa, no valida")
+    print("Edad no valida")
     edad=(int(input("Escribe tu edad: ")))
     cc+=1
     if cc==4:
-        print("numero maximo de intentos")
+        print("numero maximo de intentos")  # aqui define una cantidad maximo de intentos en un bucle indeterminado ya que se anida el while con el if
         break                               # break permite que si la condicion se cumple, el programa termina en ese momento
 print("gracias, puedes pasar")
 print("edad del usuario "+str(edad ))
+
+# otro problema
+
+print("Este programa halla la raiz cuadrada de un valor numerico")
+
+num=int(input("Escribe un numero: "))
+
+intentos=0
+
+while num<0:
+    print("el valor numerico no puede ser negativo")
+    num=int(input("introduce un numero: "))
+    intentos=intentos+1
+    if intentos==5:
+        break
+
+if intentos==5:
+    print("Lo siento, no puedo realizar el calculo")
+else:
+    print(math.sqrt(num))
